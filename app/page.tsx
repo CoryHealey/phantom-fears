6e28ec6db9346ddf47eac4afc4af94fe7104b638
Cory Healey <cory.healey@imporving.com>
2025-04-23 16:07:33 -0500

    000000 - poster

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,31 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-         <Image
-          src="/PhantomFearsArtIceHouse202511x17B.jpg"
-          alt="Phantom Fears Icehouse"
-          width={500}
-          height={500}
-          priority
-        />
+      <main
+        className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start content-center justify-center"
+      >
+        {/* <main className="w-full flex flex-col h-screen content-center justify-center"> */}
+        {/* <div className="flex justify-around justify-items-center"> */}
+          <div className="flex flex-col gap-[32px] items-center justify-center justify-items-center">
+          <div className="flex flex-col gap-[32px] items-center justify-center justify-items-center">
+            <p className="text-red-600/100 text-5xl font-bold">
+              Phantom Fears
+            </p>
+         
+          </div>
+          <div className="flex flex-col gap-[32px] items-center justify-center content-center">
+            <Image
+              src="/PhantomFearsArtIceHouse202511x17B.jpg"
+              alt="Phantom Fears Icehouse"
+              width={500}
+              height={500}
+              priority
+            />
+              <p className="w-96 text-justify">
+                Phantom Fears is Minnesota’s new Rush Tribute band. Formed after the tragic passing of Rush’s drummer and lyricist, Neil Peart, and dedicated with deep love to one of the most iconoclastic rock bands of all time, Phantom Fears debut performance is not to be missed. The Phantom Fears line up is: Chris Bates - bass, Cory Healey - drums, Toivo - guitar, Cory Eischen - keyboards, Aby Wolf - vocals.
+              </p>       
+          </div>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         {/* <a
